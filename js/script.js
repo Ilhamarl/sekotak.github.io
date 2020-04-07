a12e3f6233eb96c9f64d9d43db22e6c8b309a902
Ilham Anasruloh <ilham.anasruloh@student.uny.ac.id>
2020-04-07 20:09:47 +0700

    Perubahan Javascript

--- js/script.js
+++ js/script.js
@@ -24,55 +24,55 @@ $(document).ready(function () {
 
 
 
-// DATA
 function tampilkanSemuaMenu() {
-    $.getJSON('data/pizza.json', function (data) {
+    $.getJSON('data-menu.json', function (data) {
         let menu = data.menu;
+
         $.each(menu, function (i, data) {
-            $('#daftar-menu').append('<div class="col-md-4"><div class="card mb-3"><img src="img/menu/' + data.gambar + '" class="card-img-top"><div class="card-body"><h5 class="card-title">' + data.nama + '</h5><p class="card-text">' + data.deskripsi + '</p><h5 class="card-title">Rp. ' + data.harga + '</h5><a href="#" class="btn btn-primary">Pesan Sekarang</a></div></div></div>');
+            $('#daftar-menu').append(`
+    
+    <div class="column">
+        <div class="card-shadow">
+          
+          <div class="">
+            <figure class="image is-3by1">
+              <img src="` + data.gambar +`" alt="Ayam" style="object-fit: cover; border-radius: 4px 4px 0 0;">
+            </figure>
+          </div>
+          
+          <div class="card-content">
+
+          <div class="media">
+            <div class="media-left">
+              <div class="media-content">
+                <p class="title is-size-5 is-capitalized">
+                ` + data.nama +`
+                </p>
+                </div>
+              </div>
+            </div>
+            
+            <div class="content">
+            ` + data.deskripsi +`
+            </div>
+          </div>
+            
+            <footer class="card-footer">
+             <p class="card-footer-item">
+            Rp `+ data.harga +`
+             </p>
+              <p class="card-footer-item">
+                <a href="https://api.whatsapp.com/send?phone=6285291338935&text=Hallo%2C%0ASaya%20mau%20pesan%20Catering%20%2F%20Snack%20nya%20!&source=&data=" 
+                target="_newblank" class="button is-primary is-fullwidth">Order</a>
+              </p>
+            </footer>
+
+          </div>
+        </div>
+
+            `);
         });
     });
 }
-  
-  
-  function myResume() {
-    $('#data-googlesheet').html('');
-  
-    $.ajax({
-        url: 'http://omdbapi.com',
-        type: 'get',
-        dataType: 'json',
-        data: {
-            'apikey': 'dca61bcc'
-        },
-        success: function (result) {
-            if (result.Response == "True") {
-                let movies = result.Search;
-  
-                $.each(movies, function (i, data) {
-                    $('#movie-list').append(`
-                        <div class="col-md-4">
-                            <div class="card mb-3">
-                                <img src="${data.Poster}" class="card-img-top" alt="...">
-                                <div class="card-body">
-                                <h5 class="card-title">${data.Title}</h5>
-                                <h6 class="card-subtitle mb-2 text-muted">${data.Year}</h6>
-                                <a href="#" class="card-link see-detail" data-toggle="modal" data-target="#exampleModal" data-id="${data.imdbID}">See Detail</a>
-                                </div>
-                            </div>
-                        </div>
-                    `);
-                });
-  
-                $('#search-input').val('');
-  
-            } else {
-                $('#movie-list').html(`
-                    <div class="col">
-                        <h1 class="text-center">` + result.Error + `</h1>
-                    </div>
-                `)
-            }
-        }
-    });
-  }
+
+tampilkanSemuaMenu();
